1ede42508366ebdd5ba3285cf6b783040ac38ae0
RyelinWang <96949276+RyelinWang@users.noreply.github.com>
2025-07-14 15:29:06 +0800

    Update README.m

--- Spin-wave/README.m
+++ Spin-wave/README.m
@@ -1,1 +1,2 @@
-
+#### Spin-Wave 
+This folder contains the note for $SU(N)$ spin-wave method. The PDF file is a note about how to create your own SW code. The .m file is a MATLAB script to calculate the magnon spectrum of antiferromagnetic spin-1/2 Heisenberg chain with $SU(2)$ method.
